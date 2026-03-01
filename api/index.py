beb6a6c842911e26447c9e102dda924229606660
Gopu Varshini <gopuvarshini14@gmail.com>
2026-03-01 11:33:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
 
-app = Flask(__name__, 
+from flask import Flask, render_template
+import joblib
+
+app = Flask(__name__,
             template_folder="../templates",
             static_folder="../static")
+
+# Load model
+model = joblib.load("../models/credit_model.pkl")
 
 @app.route("/")
 def home():
