4b2f39f39d2d2df408ce757cb306dc654db07a72
Gopu Varshini <gopuvarshini14@gmail.com>
2026-03-01 12:14:54 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,13 @@
-
 from flask import Flask, render_template
 import joblib
 
-app = Flask(__name__,
-            template_folder="../templates",
-            static_folder="../static")
+app = Flask(__name__)
 
-# Load model
-model = joblib.load("../models/credit_model.pkl")
+model = joblib.load("models/credit_model.pkl")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-handler = app
+if __name__ == "__main__":
+    app.run()
