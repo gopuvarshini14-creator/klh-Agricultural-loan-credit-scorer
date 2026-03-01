7ef21279fcd68ebda7c4ca24dc934dec00527a1e
Gopu Varshini <gopuvarshini14@gmail.com>
2026-03-01 18:22:45 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -76,11 +76,11 @@ document.addEventListener("DOMContentLoaded", function () {
             };
 
             try {
-                const response = await fetch("http://127.0.0.1:5000/predict", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(data)
-                });
+            const response = await fetch("/predict", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+});
 
                 const result = await response.json();
 
